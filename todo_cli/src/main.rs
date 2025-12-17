4927709957250d3ebaf977c62c8571d49f31fb15
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-17 16:25:51 +0100

    Add: Serde cargo for Json parsing and created basic data types

--- todo_cli/src/main.rs
+++ todo_cli/src/main.rs
@@ -1,20 +1,64 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[derive(Parser)]
+#[command(name = "todo")]
+#[command(about = "Simple CLI Todo Application")]
 struct Cli {
-    /// Name of the person to greet
-    name: String,
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[derive(Subcommand)]
+enum Commands {
+    /// Add a new task
+    Add {
+        /// Title of the task
+        title: String,
+        /// Description of the task
+        description: String,
+    },
+    /// List all tasks
+    List,
+    /// Mark a task as completed
+    Complete {
+        id: u32,
+    },
+    /// Remove a task
+    Remove {
+        id: u32,
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Task {
+    id: u32,
+    title: String,
+    description: String,
+    completed: bool
 }
 
 fn main() {
     let args = Cli::parse();
 
-    for _ in 0..args.count {
-        println!("Hello, {}!", args.name);
+    match args.command {
+        Commands::Add { title, description } => {
+            let new_task = Task {
+                id: 0,
+                title,
+                description,
+                completed: false,
+            };
+            println!("Adding {} task as: {}", new_task.description, new_task.title);
+        }
+        Commands::List => {
+            println!("Listing all tasks");
+        }
+        Commands::Complete { id } => {
+            println!("Marking task {} as completed", id);
+        }
+        Commands::Remove { id } => {
+            println!("Removing task {}", id);
+        }
     }
 }
