b246c6251ede29d15e984c352449396c87692f84
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-17 17:28:01 +0100

    Add: File I/O helpers for loading and saving tasks into/from json file.

--- todo_cli/src/main.rs
+++ todo_cli/src/main.rs
@@ -1,6 +1,15 @@
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use std::path::Path;
+
+// Constant holding file name to save tasks locally
+const TODO_FILE: &str = "todo.json";
+
+
+// Struct CLI holds the command line arguments of type Commands
 #[derive(Parser)]
 #[command(name = "todo")]
 #[command(about = "Simple CLI Todo Application")]
@@ -9,6 +18,7 @@ struct Cli {
     command: Commands,
 }
 
+// Enum Commands holds the different commands for the CLI that we can use
 #[derive(Subcommand)]
 enum Commands {
     /// Add a new task
@@ -30,6 +40,7 @@ enum Commands {
     }
 }
 
+// Task struct holding the in memory representation of a task
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Task {
     id: u32,
@@ -61,4 +72,30 @@ fn main() {
             println!("Removing task {}", id);
         }
     }
+}
+
+// We need to return Box<dyn std::error::Error> because serde_json::from_reader
+// can return different error types. Saying to compiler that we can return some kind of error
+// This pattern has the trade off that we cannot match on specific error types later on
+// to handle or direct the logic accordingly. We just get a generic error.
+fn load_tasks() -> Result<Vec<Task>, Box<dyn std::error::Error>> {
+    let path = Path::new(TODO_FILE);
+    // If no file exists, create new vector in memory to start saving tasks
+    if !path.exists() {
+       return Ok(Vec::new())
+    }
+
+    // Try open file, create a BufReader ready to scoop data from drive 
+    let file = File::open(path)?; // ? operator. If fails on open return the error
+    let reader = BufReader::new(file);
+    // Start scooping data from file and deserialize into vector of tasks
+    let tasks: Vec<Task> = serde_json::from_reader(reader)?;
+    Ok(tasks)
+}
+
+fn save_tasks(tasks: &Vec<Task>) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::create(TODO_FILE)?; // If file does not exist, create it
+    let writer = BufWriter::new(file);
+    serde_json::to_writer_pretty(writer, &tasks)?;
+    Ok(())
 }
