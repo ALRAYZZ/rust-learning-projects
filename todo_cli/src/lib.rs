17b75c692b1cc56697cf2ef1e120bf01749401ed
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-18 17:38:33 +0100

    Refactor: Created TodoList struct to contain all logic of the in memory manipulation and I/O operations.

--- todo_cli/src/lib.rs
+++ todo_cli/src/lib.rs
@@ -6,6 +6,98 @@ use std::io::{BufReader, BufWriter};
 use std::path::Path;
 
 
+pub struct TodoList {
+    tasks: Vec<Task>,
+}
+
+// Represents the in memory list of tasks with methods to manipulate it
+// and perform I/O operations
+impl TodoList {
+    // Load from file into TodoList
+    // Returns a Result with either the TodoList struct or an error
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        let tasks = load_tasks()?;
+        Ok(Self { tasks })
+    }
+
+    // Internal save
+    fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        save_tasks(&self.tasks)
+    }
+
+    // Add a task to the in memory vector and save to file
+    pub fn add(&mut self, title: String, description: String)
+        -> Result<u32, Box<dyn std::error::Error>> {
+
+        // Convert into iterator, map projects(extracts) the id field from each task
+        // max returns an option of either the max value of task.ids or None if no tasks exist
+        // then we have unwrap_or(0) to return 0 if no tasks exist, and add 1 to get the next id
+        let next_id = Task::find_next_id(&self.tasks);
+        let new_task = Task::new(next_id, title, description);
+        self.tasks.push(new_task);
+        self.save()?;
+        Ok(next_id)
+    }
+
+    // List tasks from memory
+    pub fn list(&self) {
+        if self.tasks.is_empty() {
+            println!("No tasks found.");
+        } else {
+            for task in &self.tasks {
+                let status = if task.completed { "[✓]" } else { "[ ]" };
+                println!(
+                    "{} ID: {} - Title: {} | Description: {}",
+                    status, task.id, task.title, task.description
+                );
+            }
+        }
+    }
+
+    // Complete a task by id and save the updated vector to file
+    pub fn complete(&mut self, id: u32) -> Result<(), Box<dyn std::error::Error>> {
+        Task::mark_task_completed(&mut self.tasks[..], id)?;
+        self.save()?;
+        Ok(())
+    }
+
+    // Remove a task from vector by id and save the updated vector to file
+    pub fn remove(&mut self, id: u32) -> Result<(), Box<dyn std::error::Error>> {
+
+        // APPROACH USING POSITION AND REMOVE
+        // Is more performant than retain because we stop searching once we find the task
+        // also allows us to give better feedback to user
+        // But in reality the IO operations are the bottleneck, so performance difference is negligible
+        if let Some(pos) = self.tasks.iter().position(|t| t.id == id) {
+            self.tasks.remove(pos);
+            self.save()?;
+            Ok(())
+        } else {
+            Err(format!("Task {} not found", id).into())
+        }
+
+        // APPROACH USING RETAIN (NOT IN USE)
+
+        // Retain method doesn't return success or failure, it just modifies the vector in place
+        // So we need a way to know if we actually removed something
+        // We can get and before and after length of the vector
+        //let original_len = tasks.len();
+        // Keep on the vector only the tasks that do not match the id to remove
+        //tasks.retain(|task| task.id != id);
+
+        // If original length is the same as current length, means we didn't remove anything
+        //if original_len == tasks.len() {
+        //return Err(format!("Task {} not found", id).into());
+        //}
+
+        //save_tasks(&tasks)?;
+        //println!("Task {} removed successfully", id);
+        //Ok(())
+    }
+}
+
+
+
 // Task struct holding the in memory representation of a task
 // Instead of making every field pub I could implement a constructor pub fn new
 // but, then I would need to implement getters for every field if I wanted to access them outside
@@ -16,6 +108,41 @@ pub struct Task {
     pub title: String,
     pub description: String,
     pub completed: bool
+}
+
+impl Task {
+   pub fn new(id: u32, title: String, description: String) -> Self {
+        Self {
+            id,
+            title,
+            description,
+            completed: false,
+        }
+   }
+    // &[Task] is the default to pass collections as references in Rust way better than
+    // passing ownership of the vector
+    // Use &[T] slice when only need to read data(looping, searching, etc)
+    // Use &mut Vec<T> when you need to modify the collection(add, remove, update)
+    // Use Vec<T> when you need to take ownership of the collection(move it somewhere else)
+    pub fn find_next_id(tasks: &[Task]) -> u32 {
+        tasks
+            .iter()
+            .map(|task| task.id)
+            .max()
+            .unwrap_or(0) + 1
+    }
+
+    // Find method returns an Options, so we can combine it with an if let pattern
+    // so we are checking if it returned Some(task), means we found the task with that id
+    // then do something with it
+    // The find method syntax is like saying: Find a task where task.id equals the id passed
+    pub fn mark_task_completed(tasks: &mut [Task], id: u32) -> Result<(), String> {
+        tasks
+            .iter_mut()
+            .find(|task| task.id == id)
+            .map(|task| { task.completed = true })
+            .ok_or_else(|| { format!("Task with id {} not found", id) })
+    }
 }
 
 // Enum Commands holds the different commands for the CLI that we can use
--- todo_cli/src/main.rs
+++ todo_cli/src/main.rs
@@ -3,93 +3,29 @@ use clap::Parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
+    // Load tasks from file into memory
+    let mut todo_list = TodoList::load()?;
 
     match args.command {
         Commands::Add { title, description } => {
-            let mut tasks = load_tasks()?;
-            // Convert into iterator, map projects(extracts) the id field from each task
-            // max returns an option of either the max value of task.ids or None if no tasks exist
-            // then we have unwrap_or(0) to return 0 if no tasks exist, and add 1 to get the next id
-            let next_id = tasks
-                .iter()
-                .map(|task| task.id)
-                .max().unwrap_or(0) + 1;
-
-            let new_task = Task {
-                id: next_id,
-                title,
-                description,
-                completed: false,
-            };
-
-            tasks.push(new_task);
-            save_tasks(&tasks)?; // Passing reference is more performant and a default in Rust
+            // Adds task and returns next id
+            let next_id = todo_list.add(title, description)?;
             println!("Task added successfully with ID: {}", next_id);
             Ok(())
         }
         Commands::List => {
-            let tasks = load_tasks()?;
-            if tasks.is_empty() {
-                println!("No tasks found");
-            } else {
-                for task in tasks {
-                    let status = if task.completed { "[✓]" } else { "[ ]" };
-                    println!(
-                        "{} ID: {} - Title: {} | Description: {}",
-                        status, task.id, task.title, task.description
-                    );
-                }
-            }
+            todo_list.list();
             Ok(())
         }
         Commands::Complete { id } => {
-            let mut tasks = load_tasks()?;
-            // Find method returns an Options, so we can combine it with an if let pattern
-            // so we are checking if it returned Some(task), means we found the task with that id
-            // then do something with it
-            // The find method syntax is like saying: Find a task where task.id equals the id passed
-            if let Some(task) = tasks.iter_mut().find(|task| task.id == id) {
-                task.completed = true;
-                save_tasks(&tasks)?;
-                println!("Task {} marked as completed", id);
-                Ok(())
-            } else {
-                Err(format!("Task {} not found", id).into())
-            }
+            todo_list.complete(id)?;
+            println!("Task {} marked as completed", id);
+            Ok(())
         }
         Commands::Remove { id } => {
-            let mut tasks = load_tasks()?;
-
-            // APPROACH USING POSITION AND REMOVE
-            // Is more performant than retain because we stop searching once we find the task
-            // also allows us to give better feedback to user
-            // But in reality the IO operations are the bottleneck, so performance difference is negligible
-            if let Some(task) = tasks.iter().position(|task| task.id == id) {
-                tasks.remove(task);
-                save_tasks(&tasks)?;
-                println!("Task {} removed successfully", id);
-                Ok(())
-            } else {
-                Err(format!("Task {} not found", id).into())
-            }
-
-            // APPROACH USING RETAIN
-
-            // Retain method doesn't return success or failure, it just modifies the vector in place
-            // So we need a way to know if we actually removed something
-            // We can get and before and after length of the vector
-            //let original_len = tasks.len();
-            // Keep on the vector only the tasks that do not match the id to remove
-            //tasks.retain(|task| task.id != id);
-
-            // If original length is the same as current length, means we didn't remove anything
-            //if original_len == tasks.len() {
-                //return Err(format!("Task {} not found", id).into());
-            //}
-
-            //save_tasks(&tasks)?;
-            //println!("Task {} removed successfully", id);
-            //Ok(())
+            todo_list.remove(id)?;
+            println!("Task {} removed successfully", id);
+            Ok(())
         }
     }
 }
