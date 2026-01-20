5114f6d3795ca555a7cc6614f0b190cfd5f08ac1
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-01-20 15:46:16 +0100

    add: added dependencies

--- wgpu_rust/src/main.rs
+++ wgpu_rust/src/main.rs
@@ -1,3 +1,35 @@
+use std::sync::Arc;
+use winit::{
+    application::ApplicationHandler, event::*, event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey}, window::Window
+};
+
+
+// If compiling to WebAssembly, include the wasm_bindgen crate
+// wasm_is used for interoperability between Rust and JavaScript
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+pub struct State {
+    window: Arc<Window>,
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self {
+            window,
+        })
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {
+        // Handle window resizing here
+    }
+
+    pub fn render(&mut self) {
+        self.window.request_redraw();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Test wgpu with winit!");
 }
