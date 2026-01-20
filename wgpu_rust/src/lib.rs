cc063d61a885bd9dc24c83d4ae64b89986f017d6
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-01-20 18:10:49 +0100

    feat: implement cross-platform windowing and wasm-pack support

--- wgpu_rust/src/lib.rs
+++ wgpu_rust/src/lib.rs
@@ -166,6 +166,11 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn main() {
-    println!("Test wgpu with winit!");
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
+    console_error_panic_hook::set_once();
+    run().unwrap_throw();
+
+    Ok(())
 }
