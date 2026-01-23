8cc8dddaf4b5fa262ecbea05f2fe437a99470e93
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-01-23 12:56:33 +0100

    doc: add study notations on the render pipeline

--- wgpu_rust/src/lib.rs
+++ wgpu_rust/src/lib.rs
@@ -115,35 +115,44 @@ impl State {
 
         // Defines the fixed-function state and links shaders, tells GPU how to transform vertices
         // Combines shader logic (VS and FS) with hardware settings into a single object to use
+        // Factory assembly line, from raw data to the final pixel color before we start
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
+            // 1st Programmable step, vertex shader
+            // Transform raw input data into Clip Space positions
             vertex: wgpu::VertexState {
                 module: &shader,
-                entry_point: Some("vs_main"), // 1.
-                buffers: &[], // 2.
+                entry_point: Some("vs_main"), // Shader function to use as entry point
+                buffers: &[],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             },
-            fragment: Some(wgpu::FragmentState { // 3.
+            // 2nd Programmable step, determines the color of every pixel inside the triangles.
+            fragment: Some(wgpu::FragmentState {
                 module: &shader,
                 entry_point: Some("fs_main"),
-                targets: &[Some(wgpu::ColorTargetState { // 4.
+                targets: &[Some(wgpu::ColorTargetState {
                     format: config.format,
                     blend: Some(wgpu::BlendState::REPLACE),
                     write_mask: wgpu::ColorWrites::ALL,
                 })],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             }),
+            // Fixed function states not programmable by shaders
+            // Switches and configurations for parts of the GPU pipeline
+            // Takes individual points from vertex shader and assembles them into shapes
+            // (triangles, lines, etc) also handles culling and front face definition
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                polygon_mode: wgpu::PolygonMode::Fill,
+                strip_index_format: None, // Used for special topologies like triangle strips
+                front_face: wgpu::FrontFace::Ccw, // Counter-clockwise vertices are front face
+                cull_mode: Some(wgpu::Face::Back), // Cull back faces
+                polygon_mode: wgpu::PolygonMode::Fill, // Fill, Line, Point
                 unclipped_depth: false,
                 conservative: false,
             },
             depth_stencil: None,
+            // How to handle multi-sampling (anti-aliasing)
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
