8e59564e156701bc48714f254cb2be65c9e618eb
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-01-26 16:58:07 +0100

    doc: add study notations on the use of bytemuck for GPU buffers

--- wgpu_rust/src/graphics/buffers.rs
+++ wgpu_rust/src/graphics/buffers.rs
@@ -1,5 +1,6 @@
 use wgpu::util::DeviceExt;
 
+// Create a vertex buffer from given vertices
 pub fn create_vertex_buffer(device: &wgpu::Device, vertices: &[crate::graphics::vertex::Vertex])
     -> wgpu::Buffer {
     device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
--- wgpu_rust/src/graphics/vertex.rs
+++ wgpu_rust/src/graphics/vertex.rs
@@ -1,6 +1,13 @@
+// repr(C) ensures the struct has a predictable memory layout C style so no unexpected padding or
+// reordering occurs, important for GPU data
+
+// Pod guarantess that the struct can be safely treated as a plain byte array
+// Zeroable allows the struct to be initialized to all zeros safely
+
+// GPU doesnt understand Rust structs directly. For GPU a buffer is a long sequence of bytes u8
+// Thats why we need the bytemuck crate to convert between Rust structs and byte arrays
 #[repr(C)]
 #[derive(Debug, Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
-
 pub struct Vertex {
     position: [f32; 3], // Fixed size array for position (x, y, z)
     color: [f32; 3], // Fixed size array for color (r, g, b)
