c4ffb049887b4065dc7ee207facc8e453d80ae56
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-01-26 14:59:41 +0100

    refactor: remove WASM support - Removed all the conditional cfg code and needed dependencies for WASM compilation

--- wgpu_rust/src/lib.rs
+++ wgpu_rust/src/lib.rs
@@ -4,12 +4,6 @@ use winit::{
     keyboard::{KeyCode, PhysicalKey}, window::Window
 };
 
-
-// If compiling to WebAssembly, include the wasm_bindgen crate
-// wasm_is used for interoperability between Rust and JavaScript
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-
 // THE ENGINE
 // GPU context. Live inside APP, holds device, queue, surface, config, translates logic into
 // binary commands for GPU
@@ -35,8 +29,6 @@ impl State {
         let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
             #[cfg(not(target_arch = "wasm32"))]
             backends: wgpu::Backends::PRIMARY,
-            #[cfg(target_arch = "wasm32")]
-            backends: wgpu::Backends::GL,
             ..Default::default()
         });
 
@@ -186,7 +178,6 @@ impl State {
         }
     }
 
-
     // Handle keyboard input events
     fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
         match (code, is_pressed) {
@@ -275,19 +266,13 @@ impl State {
 // Manages OS lifecycle. Speaks to winit to create windows, handle events, etc
 // Does not care about rendering, but that there is a window to render to
 pub struct App {
-    #[cfg(target_arch = "wasm32")]
-    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
     state: Option<State>,
 }
 
 impl App  {
-    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
-        #[cfg(target_arch = "wasm32")]
-        let proxy = Some(event_loop.create_proxy());
+    pub fn new() -> Self {
         Self {
             state: None,
-            #[cfg(target_arch = "wasm32")]
-            proxy,
         }
     }
 }
@@ -299,59 +284,11 @@ impl ApplicationHandler<State> for App {
         #[allow(unused_mut)] // To avoid warnings on non-wasm32 targets
         let mut window_attributes = Window::default_attributes();
 
-        #[cfg(target_arch = "wasm32")]
-        {
-            use wasm_bindgen::JsCast;
-            use winit::platform::web::WindowAttributesExtWebSys;
-
-            const CANVAS_ID: &str = "canvas";
-
-            // On web, we need to get the canvas element from the HTML document
-            // and set it in the window attributes, then create the window
-            let window = wgpu::web_sys::window().unwrap_throw();
-            let document = window.document().unwrap_throw();
-            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
-            let html_canvas_element = canvas.unchecked_into();
-            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
-        }
-
         // Create the window
         let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
 
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            // If we are not on web use pollster
-            self.state = Some(pollster::block_on(State::new(window)).unwrap());
-        }
-
-        #[cfg(target_arch = "wasm32")]
-        {
-            // Run the future async and use proxy to send results to event loop
-            if let Some(proxy) = self.proxy.take() {
-                wasm_bindgen_futures::spawn_local(async move {
-                    assert!(proxy
-                        .send_event(
-                            State::new(window)
-                                .await
-                                .expect("Failed to create canvas!")
-                        )
-                    .is_ok());
-                });
-            }
-        }
-    }
-
-    #[allow(unused_mut)]
-    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
-        #[cfg(target_arch = "wasm32")]
-        {
-            event.window.request_redraw();
-            event.resize(
-                event.window.inner_size().width,
-                event.window.inner_size().height,
-            );
-        }
-        self.state = Some(event);
+        // If we are not on web use pollster
+        self.state = Some(pollster::block_on(State::new(window)).unwrap());
     }
 
     // Handle window events like resize, close, redraw, keyboard input
@@ -403,30 +340,12 @@ impl ApplicationHandler<State> for App {
 
 // Setup logging and run the event loop
 pub fn run() -> anyhow::Result<()> {
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        env_logger::init();
-    }
-    #[cfg(target_arch = "wasm32")]
-    {
-        console_log::init_with_level(log::Level::Info).unwrap_throw();
-    }
+
+    env_logger::init();
 
     let event_loop = EventLoop::with_user_event().build()?;
-    let mut app = App::new(
-        #[cfg(target_arch = "wasm32")]
-        &event_loop,
-    );
+    let mut app = App::new();
     event_loop.run_app(&mut app)?;
-
-    Ok(())
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(start)]
-pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
-    console_error_panic_hook::set_once();
-    run().unwrap_throw();
 
     Ok(())
 }
