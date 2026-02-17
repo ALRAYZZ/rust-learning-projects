387f3a326b6e79fc74d7f15e70340c4dd275dfa9
Alejandro Montaner Roig <alroigcode@gmail.com>
2026-02-17 15:11:45 +0100

    refactor: transactions use a human-redable name, and the global ledger maps that string int o a public key

--- wonderchain/src/main.rs
+++ wonderchain/src/main.rs
@@ -4,7 +4,7 @@ use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
 const UNIQUE_SUPPLY: u64 = 1000000;
 
 struct Transaction {
-    sender_public_key: VerifyingKey,
+    sender_name: String,
     receiver_name: String,
     amount: u64,
     // Only private key-holder can produce signature, anyone with the public key can verify it,
@@ -15,46 +15,45 @@ struct Transaction {
 
 // Global Ledger
 struct State {
-    // Map public key (Authority) -> Balance
-    balances: HashMap<[u8; 32], u64> // Every key must be a 32-byte array (Ed25519 public key size)
+    // Names -> Balance
+    balances: HashMap<String, u64>, // Every key must be a 32-byte array (Ed25519 public key size)
+    // Name -> Public Key (Phonebook)
+    names: HashMap<String, VerifyingKey>,
 }
 
 impl State {
-    fn new(god_public_key: VerifyingKey) -> Self {
+    fn new(god_name: &str, god_key: VerifyingKey) -> Self {
         let mut balances = HashMap::new();
+        let mut names = HashMap::new();
         // Rule 1 - All coins exists at birth in GOD
-        balances.insert(god_public_key.to_bytes(), UNIQUE_SUPPLY);
+        balances.insert(god_name.to_string(), UNIQUE_SUPPLY);
+        names.insert(god_name.to_string(), god_key);
 
-
-        State { balances }
+        State { balances, names }
     }
 
-    fn verify_and_apply_transaction(&mut self, tx: Transaction) -> Result<(), String> {
-        // Create message that was signed (amount + receiver)
-        // FRAGILE, ambiguity, amount = 12, reciever = 3 -> "123" as amount = 1, receiver = 23 -> "123"
-        // We need canonicalization, or better yet, a structured message format (e.g. JSON, protobuf)
-        let message = format!("{}{}", tx.amount, tx.receiver_name);
+    fn process_transaction(&mut self, tx: Transaction) -> Result<(), String> {
+        // Look up sender key
+        let sender_key = self.names.get(&tx.sender_name)
+            .ok_or("Sender does not exist!");
 
-        // Also receiver should have same addressing system as the ledger.
-        // Receiver should be VerifyingKey or at least a hash of it, not a free-form string.
+        // Verify signature
+        let message = format!("{}{}{}", tx.sender_name, tx.receiver_name, tx.amount);
+        sender_key?.verify(message.as_bytes(), &tx.signature)
+            .map_err(|_| "Invalid signature!")?;
 
-        // Cryptographic Check: Did sender sign this?
-        tx.sender_public_key
-            .verify(message.as_bytes(), &tx.signature)
-            .map_err(|_| "Invalid signature".to_string())?;
-
-        // Balance check
-        let sender_bytes = tx.sender_public_key.to_bytes();
-        let sender_balance = self.balances.get(&sender_bytes).unwrap_or(&0);
-
-        if *sender_balance < tx.amount {
-            return Err("Insufficient balance".to_string());
+        // Check Balance
+        let sender_balance = self.balances.get(&tx.sender_name).cloned().unwrap_or(0);
+        if sender_balance < tx.amount {
+            return Err("Insufficient balance!".to_string());
         }
 
-        // Apply state change
-        *self.balances.get_mut(&sender_bytes).unwrap() -= tx.amount;
-        println!("Successfully moved {} to {}", tx.amount, tx.receiver_name);
+        // Update state
+        *self.balances.get_mut(&tx.sender_name).unwrap() -= tx.amount;
 
+        // If receiver does not exist, we create them
+        // We might requite separate Register Transaction
+        *self.balances.entry(tx.receiver_name.clone()).or_insert(0) += tx.amount;
 
         Ok(())
     }
@@ -68,28 +67,37 @@ fn main() {
     // SigningKey is the private key, who has it can sign transactions, meaning can spend the coin.
     // VerifyingKey is the public key, which is used to verify signatures, and also serves as the identity/address in the ledger.
     // Private for authorization, public for identity and verification.
+
+    // Setup GOD
     let god_private_key = SigningKey::generate(&mut csprng);
     let god_public_key = god_private_key.verifying_key();
-
-    // Initialize State with GOD's public key, giving GOD all the coins at birth.
-    let mut wonderchain = State::new(god_public_key);
+    let mut wonderchain = State::new("GOD", god_public_key);
 
     // Create Signed Transaction
     let amount = 500;
+    let sender = "GOD".to_string();
     let receiver = "ray".to_string();
-    let message = format!("{}{}", amount, receiver);
+
+    // Message must include sender name to prevent replay attacks
+    let message = format!("{}{}{}", sender, receiver, amount);
     let signature = god_private_key.sign(message.as_bytes());
 
     let tx = Transaction {
-        sender_public_key: god_public_key,
+        sender_name: sender,
         receiver_name: receiver,
         amount,
         signature
     };
 
-    // Process
-    match wonderchain.verify_and_apply_transaction(tx) {
-        Ok(_) => println!("Transaction processed successfully!"),
+    // Execute
+    match wonderchain.process_transaction(tx) {
+        Ok(_) => {
+            println!("Transaction processed successfully!");
+            println!("God: {} | Alice: {}",
+            wonderchain.balances["GOD"],
+            wonderchain.balances["ray"]
+            );
+        },
         Err(e) => println!("Transaction failed: {}", e),
     }
 }
